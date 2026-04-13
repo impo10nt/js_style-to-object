6379c66a6fea86f20f07fdd2d30d9a62c6a5d08f
{bogdan} <{bogdanmihajlenko7@gmail.com}>
2026-04-13 20:11:38 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,26 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const declarations = sourceString.split(';');
+
+  for (let i = 0; i < declarations.length; i++) {
+    const declaration = declarations[i].trim();
+
+    if (declaration.length === 0) {
+      continue;
+    }
+
+    const colonIndex = declaration.indexOf(':');
+
+    const key = declaration.slice(0, colonIndex).trim();
+
+    const value = declaration.slice(colonIndex + 1).trim();
+
+    result[key] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
