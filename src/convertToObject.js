953ff4209fc8eecd8a32031cf992ce5f6178d4bf
{bogdan} <{bogdanmihajlenko7@gmail.com}>
2026-04-14 22:34:20 +0300

    new commit

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,26 +7,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
-  const result = {};
-  const declarations = sourceString.split(';');
+  return sourceString.split(';').reduce((acc, declaration) => {
+    const item = declaration.trim();
 
-  for (let i = 0; i < declarations.length; i++) {
-    const declaration = declarations[i].trim();
+    if (item.length > 0) {
+      const colonIndex = item.indexOf(':');
+      const key = item.slice(0, colonIndex).trim();
+      const value = item.slice(colonIndex + 1).trim();
 
-    if (declaration.length === 0) {
-      continue;
+      acc[key] = value;
     }
 
-    const colonIndex = declaration.indexOf(':');
-
-    const key = declaration.slice(0, colonIndex).trim();
-
-    const value = declaration.slice(colonIndex + 1).trim();
-
-    result[key] = value;
-  }
-
-  return result;
+    return acc;
+  }, {});
 }
 
 module.exports = convertToObject;
